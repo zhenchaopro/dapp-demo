257c03f4a5cea370e69a4da891020f6e412afe80
zc <fanzc.daily@gmail.com>
2022-10-26 01:32:04 +0800

    Deploy

---
+++ out/_next/static/sM_ixtMfshk4X1PNJZl1d/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/244-b440508c4117b3a5bd5c.js","static/chunks/pages/index-a26a9427441d6463456e.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
